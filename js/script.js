7825067f8067708394acb0d30f984ff80fae4c49
Akhilesh kushwah <90171723+Akhilesh17-Tech@users.noreply.github.com>
2022-01-28 09:09:52 +0530

    minor bug fixed

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ function getmealList() {
                 </div>
                 <div class="title">${meal.strMeal}</div>
                 <div class="btn-box">
-                    <button type="submit" onclick="functionToExecute()">Favourite</button>
-                    <button type="submit">Get Recipe</button>
+                     <button type="submit" onclick="functionToExecute(${meal.idMeal})"><i class="far fa-heart"></i> Favourite</button>
+                    <button type="submit"><a href="mealDetail.html?id=${meal.idMeal}" value="${meal.idMeal}">Get Recipe</a></button>
                 </div>
             </div>`;
         });
